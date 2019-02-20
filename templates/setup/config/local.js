1ac83192906ea127d3ba7c08b0e18f4da0632ce2
Johnny Hausman <johnny@hausmanfamily.com>
2019-02-21 01:35:53 +0700

    [add] setup() command now includes setting up the bot_manager settings in config/local.js

--- templates/setup/config/local.js
+++ templates/setup/config/local.js
@@ -12,12 +12,18 @@ module.exports = {
   // Any configuration settings may be overridden below, whether it's built-in Sails
   // options or custom configuration specifically for your app (e.g. Stripe, Mailgun, etc.)
 
-  // bot_manager:
-  // define the connections between our bot_manager and the host command
-  // processor.
+  /**
+   * bot_manager:
+   * define the connections between our bot_manager and the host command
+   * processor.
+   */
   bot_manager: {},
+  /* end bot_manager */
 
-  // notification_email
-  // our smtp email service
+  /**
+   * notification_email
+   * our smtp email service
+   */
   notification_email: {}
+  /* end notification_email */
 };

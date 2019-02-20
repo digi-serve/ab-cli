dc25e059382784e76165c1358d528690c104f6d5
Johnny Hausman <johnny@hausmanfamily.com>
2019-02-20 17:22:01 +0700

    [add] setup command now copies templates and also sets up the ssl settings.

---
+++ templates/setup/config/local.js
@@ -0,0 +1,23 @@
+/**
+ * Local environment settings
+ *
+ * Use this file to specify configuration settings for use while developing
+ * the app on your personal system.
+ *
+ * For more information, check out:
+ * https://sailsjs.com/docs/concepts/configuration/the-local-js-file
+ */
+
+module.exports = {
+  // Any configuration settings may be overridden below, whether it's built-in Sails
+  // options or custom configuration specifically for your app (e.g. Stripe, Mailgun, etc.)
+
+  // bot_manager:
+  // define the connections between our bot_manager and the host command
+  // processor.
+  bot_manager: {},
+
+  // notification_email
+  // our smtp email service
+  notification_email: {}
+};

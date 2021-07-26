2a551d29eb275c9c48dd189788b029dfd3bfcea6
Johnny Hausman <johnny@hausmanfamily.com>
2021-07-26 14:09:29 +0700

    [add] relocating slackbot connection to the system_monitor.

--- templates/setup/[stack]_system_monitor.js
+++ templates/setup/[stack]_system_monitor.js
@@ -24,7 +24,10 @@ function onExit(code) {
 }
 process
    .on("SIGINT", () => {
-      onExit("SIGINT");
+      // ignore SIGINT since ctl-c out of UP.sh command will
+      // pass this along to this process which should keep
+      // running.
+      console.log("Caught a SIGINT. Ignoring.");
    })
    .on("SIGTERM", () => {
       onExit("SIGTERM");
@@ -212,3 +215,159 @@ fs.stat(SOCKETFILE, function (err /*, stats */) {
 
 // now setup our checkRunning() operation
 setInterval(checkRunning, 5 * 1000);
+
+/////
+///// Slack Bot
+/////
+///// The @slack/bolt library isn't working properly from within a docker swarm.
+///// So we are folding that connection in here for now.
+
+var servicesToWatch = [];
+// {array}
+// an array of all the digiserve/(service:tag) that we are running in our
+// docker-compose.yml file.
+
+var serviceNames = {};
+// {lookup hash}  { docker/service:tag  :  docker_service_name }
+// a quick lookup hash with the names of the current docker stack + service
+// name for each of our servicesToWatch.
+
+var stack = "ab";
+// {string}
+// the docker stack reference for this running stack.
+
+function lookupServices() {
+   var allServices = [];
+   var contents = fs
+      .readFileSync(
+         //// TODO: replace this:
+         // path.join(process.cwd(), "..", "test", "abC2", "docker-compose.yml")
+         //// to this:
+         path.join(process.cwd(), "docker-compose.yml")
+      )
+      .toString();
+
+   var serviceExp = new RegExp("image: (.*)", "g");
+   var service;
+   while ((service = serviceExp.exec(contents))) {
+      allServices.push(service[1]);
+   }
+   return allServices;
+}
+
+function lookupServiceName(imageName) {
+   if (!serviceNames[imageName]) {
+      var command = `docker service ls | grep "${imageName}" | grep " ${stack}_" | awk '{print $2}'`;
+      console.log("lookupServiceName ::: ", command);
+      serviceNames[imageName] = shell.exec(command).stdout;
+      console.log("lookupServiceName --> ", serviceNames[imageName]);
+   }
+   return serviceNames[imageName];
+}
+
+function lookupStack() {
+   var foundStack = stack;
+
+   var contents = fs.readFileSync(path.join(process.cwd(), "UP.sh")).toString();
+
+   var serviceExp = new RegExp("File (.*)", "g");
+   var results;
+   while ((results = serviceExp.exec(contents))) {
+      foundStack = results[1];
+   }
+
+   return foundStack;
+}
+
+function pullDockerUpdate(msg) {
+   var repo = null;
+
+   ////
+   //// incoming DockerHub notification:
+   ////
+   // {
+   //   type: 'message',
+   //   subtype: 'bot_message',
+   //   text: '',
+   //   ts: '1626851514.000800',
+   //   bot_id: 'B0289N0E3PZ',
+   //   attachments: [
+   //     {
+   //       author_name: 'digiserve',
+   //       title: "digiserve/ab-definition-manager:develop: Build in 'develop' (f151fad7)",
+   //       id: 1,
+   //       title_link: 'https://hub.docker.com/repository/registry-1.docker.io/digiserve/ab-definition-manager/builds/ac2a4e26-3737-40a0-b1bf-209989bccc2f',
+   //       color: '2eb886',
+   //       mrkdwn_in: [Array]
+   //     }
+   //   ],
+   //   channel: 'C028NM9QXPT',
+   //   event_ts: '1626851514.000800',
+   //   channel_type: 'channel'
+   // }
+
+   if (msg.subtype == "bot_message") {
+      (msg.attachments || []).forEach((a) => {
+         (servicesToWatch || []).forEach((s) => {
+            if (a.title.indexOf(s) > -1) {
+               repo = s;
+            }
+         });
+      });
+   }
+
+   return repo;
+}
+
+console.log("Monitoring Docker Containers:");
+console.log(servicesToWatch);
+
+if (!config.slackBot.enable) {
+   console.log("... slackBot connection DISABLED.");
+} else {
+   servicesToWatch = lookupServices();
+   stack = lookupStack();
+
+   const { App } = require("@slack/bolt");
+
+   const app = new App({
+      token: config.slackBot.botToken,
+      signingSecret: config.slackBot.signingSecret,
+      socketMode: config.slackBot.socketMode,
+      appToken: config.slackBot.appToken,
+      logLevel: config.slackBot.logLevel, // "debug",
+   });
+
+   // Listens to incoming messages
+   app.message(/.*/, async ({ message, say }) => {
+      // console.log(message);
+
+      // find out if this is a Docker Update on one of our running containers
+      var updatedRepo = pullDockerUpdate(message);
+      if (updatedRepo) {
+         var serviceName = lookupServiceName(updatedRepo);
+         if (serviceName) {
+            await say(
+               `... ${config.slackBot.botName} : reloading ${updatedRepo} `
+            );
+
+            var command = `docker service update --image ${updatedRepo} ${serviceName}`;
+            console.log("command: ", command);
+            shell.exec(command, (/* code, stdout, stderr */) => {
+               say(`... ${config.slackBot.botName} : finished `);
+            });
+         } else {
+            console.log(
+               `::: could not resolve ${updatedRepo} into a running service ::: `
+            );
+         }
+      }
+   });
+
+   (async () => {
+      // Start your app
+      await app.start(config.slackBot.port);
+
+      console.log("SlackBot connection is running!");
+   })();
+}

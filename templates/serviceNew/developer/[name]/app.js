34cd0eb16b9d57b6318552e108fde0f763319252
Johnny Hausman <johnny@hausmanfamily.com>
2021-09-01 17:26:17 +0700

    [add] temporarily expose a tenant add command

--- templates/serviceNew/developer/[name]/app.js
+++ templates/serviceNew/developer/[name]/app.js
@@ -7,4 +7,5 @@ const AB = require("ab-utils");
 var controller = AB.controller("<%= name %>");
 // controller.afterStartup((req, cb)=>{ return cb(/* err */) });
 // controller.beforeShutdown((req, cb)=>{ return cb(/* err */) });
+// controller.waitForDB = true; // {bool} wait for mysql to be accessible before .init() is processed
 controller.init();
